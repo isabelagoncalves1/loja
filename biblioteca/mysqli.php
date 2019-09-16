e7b0ecd5dbf671a85a1c01538593da82ea131aec
isabelagoncalves1 <47746608+isabelagoncalves1@users.noreply.github.com>
2019-09-16 17:31:41 -0300

    Update mysqli.php

--- biblioteca/mysqli.php
+++ biblioteca/mysqli.php
@@ -1,7 +1,7 @@
 <?php
 
 function conn() {
-    $cnx = mysqli_connect("localhost", "id9920439_isabela_pw2", "0doisdez2002@", "id9920439_webloja");
+    $cnx = mysqli_connect("localhost", "id10888147_isabela", "0doisdez2002@", "id10888147_webloja");
     //$cnx = mysqli_connect("ftpupload.net", "epiz_24472616", "7inGUgkBKCSvNNb", "epiz_24472616_webloja");
     //$cnx = mysqli_connect("localhost", "root", "", "webloja");
     if (!$cnx) die('Deu errado a conexao!');
