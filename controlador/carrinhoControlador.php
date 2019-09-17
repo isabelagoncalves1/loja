a081f8f49ed127ed68cb1a7921f7e711b89a6da8
isabelagoncalves1 <47746608+isabelagoncalves1@users.noreply.github.com>
2019-09-17 08:55:32 -0300

    Update carrinhoControlador.php

--- controlador/carrinhoControlador.php
+++ controlador/carrinhoControlador.php
@@ -1,45 +1,38 @@
 <?php
-
 require_once "modelo/produtoModelo.php";
 require_once "servico/correiosServico.php";
 
-function adicionar($idProduto) {
-    $produtos = array();
-     $produtos = $_SESSION["car"];
-    if(isset($_SESSION["car"])){
-        $produtos = $_SESSION["car"];
-        echo "if";
-    }// tem um else aqui
-    
-    print_r($produtos);
-    $produtos[] = $idProduto;
-    print_r($produtos);
-//    $produtos[] = pegarProdutoId($idProduto);
-    $_SESSION["car"] = $produtos;
-    print_r($_SESSION["car"]);
-//    redirecionar("carrinho/listar");
+function comprar($idProduto) {
+    $_SESSION["carrinho"][] = $idProduto;
+    redirecionar("./carrinho/exibirSession");
 }
-
-function listar() {
-    print_r($_SESSION["car"]);
-    $dados['produtos'] = $_SESSION["car"];
-    echo "oi";
+function exibirSession() { # listarCarrinho
+    $lista= array();
+    
+    for($i=0; $i<count($_SESSION["carrinho"]); $i++){
+        $id=$_SESSION["carrinho"][$i];
+        $produto = pegarProdutoId($id);
+        $lista[]=$produto;
+    }
+    $dados["produto"] = $lista;
     exibir("paginas/carrinho", $dados);
 }
-
 function deletar() {
+    unset($_SESSION["carrinho"]);
+    session_destroy();
     unset($_SESSION['carrinho']);
     redirecionar("produto/listarprodutos");
 }
-
-function removerProduto($id) {
-    $produtos = $_SESSION['carrinho'];
-    foreach ($produtos as $key => $produto) {
-        if ($produto['idProduto'] == $id) {
-            unset($produtos[$key]);
-        }
-    }
-    $_SESSION['carrinho'] = $produtos;
-    redirecionar("carrinho/listar");
+function removerItem() {
+    $listaIds = $_SESSION["carrinho"];
+    for ($i = 0; $i < count($produtos); $i++) {
+        if ($idElementoDeleta == $produtos[$i]) {
+            echo "achei!";
+            echo "O elemento a deletar é de indice $i";
+            //deletar
+            unset($produtos[$i]);
+		}
+	}
 }
-
+ 
+?>
