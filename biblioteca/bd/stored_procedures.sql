154ace503efd3554920cfb2f0973994188c642a5
isabelagoncalves1 <47746608+isabelagoncalves1@users.noreply.github.com>
2019-11-29 07:50:42 -0300

    Update stored_procedures.sql

--- biblioteca/bd/stored_procedures.sql
+++ biblioteca/bd/stored_procedures.sql
@@ -10,12 +10,30 @@ Delimiter $$
     END; $$
 DELIMITER ;
 
+        
+		DROP PROCEDURE IF EXISTS sp_PedidosPorCliente;
+		DELIMITER $$
+			CREATE PROCEDURE sp_PedidosPorCliente (v_idCliente BIGINT(11))
+			BEGIN
+				SELECT *
+				FROM pedido
+				WHERE idCliente = v_idCliente
+				ORDER BY dataCompra;
+			END; $$
+		DELIMITER ;
 
-DROP PROCEDURE IF EXISTS sp_Faturamento;
-Delimiter $$
-    CREATE PROCEDURE sp_Faturamento (v_periodo VARCHAR(10))
-    BEGIN
-        IF () THEN
-        END IF;
-    END; $$
-DELIMITER ;
+
+		DROP PROCEDURE IF EXISTS sp_PedidosPorMunicipio;
+		DELIMITER $$
+			CREATE PROCEDURE sp_PedidosPorMunicipio (v_cidade VARCHAR(30))
+			BEGIN
+				SELECT pedido.*
+				FROM pedido
+				INNER JOIN cadastrocliente
+				ON pedido.idCliente = usuario.idCliente
+				INNER JOIN endereco
+				ON usuario.idCliente = endereco.idCliente
+				WHERE endereco.cidade = v_cidade;
+			END; $$
+		DELIMITER ;
+
